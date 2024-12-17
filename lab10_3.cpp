6e38b6356a5c8a70e4ae7ee8f350727ae0b2ab3b
zewexqa <domtui555@gmail.com>
2024-12-17 23:06:03 +0700

    lab10_3.cpp commited

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,36 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    float sum = 0.0;
+    float sum_of_square = 0.0;
+    string textline;
+
+    ifstream source("score.txt");
+    while (getline(source, textline))
+    {
+        float value = stof(textline);
+        sum += value;
+        sum_of_square += value * value;
+        count++;
+    }
+
+    source.close();
+
+    float mean = sum / count;
+    float standard_deviation = sqrt((sum_of_square / count) - (mean * mean));
+
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << standard_deviation << "\n";
+
+    return 0;
+}
